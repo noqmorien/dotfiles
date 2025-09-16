c4b3d9ef528422eca6d8675d85a1bda137759125
noqmorien <cowretwc@gmail.com>
2025-09-16 12:21:29 +0800

    feat add fish
    Signed-off-by: noqmorien <cowretwc@gmail.com>

---
+++ fish/config.fish
@@ -0,0 +1,51 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_user_key_bindings
+    fish_default_key_bindings -M insert
+    fish_vi_key_bindings --no-erase insert
+end
+
+function setup_user_path
+    fish_add_path ~/.local/bin
+    fish_add_path ~/.cargo/bin
+    fish_add_path ~/.bun/bin
+end
+
+function setup_user_env
+end
+
+function setup_aliases
+
+    if command -q tmux
+        alias tm="tmux"
+        alias tma="tmux attach"
+    end
+
+    if command -q bat
+        alias cat="bat --paging=never --style=plain --color=auto"
+    end
+
+    if command -q exa
+        alias ll="exa --git --git-repos --icons --long --header --smart-group"
+        alias ls="exa --git --git-repos --icons --smart-group"
+    end
+end
+
+function fish_greeting
+
+    setup_user_env
+    setup_user_path
+    setup_aliases
+
+    if command -q starship
+        starship init fish | source
+    end
+
+    if command -q starship
+        fastfetch
+    end
+end
+
+
